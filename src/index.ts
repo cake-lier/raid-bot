c64a878b0039716d9d52022e934ce120b78b17b0
Matteo Castellucci <matteo.castellucci@outlook.com>
2024-07-02 17:23:15 +0200

    fix: limit bot commands to groups, refactor environment variables check

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import { Telegraf } from "telegraf";
 import { Model } from "./model";
 
 async function main() {
-    const dbUsername = process.env["DB_USERNAME"]!;
-    const dbPassword = process.env["DB_PASSWORD"]!;
-    const appName = process.env["APP_NAME"]!;
-    const dbName = process.env["DB_NAME"]!;
-    const botToken = process.env["BOT_TOKEN"]!;
+    const dbUsername = process.env["DB_USERNAME"];
+    const dbPassword = process.env["DB_PASSWORD"];
+    const appName = process.env["APP_NAME"];
+    const dbName = process.env["DB_NAME"];
+    const botToken = process.env["BOT_TOKEN"];
+    if (!dbUsername || !dbPassword || !appName || !dbName || !botToken) {
+        console.error("Some environment variable was not provided, aborting");
+        return;
+    }
     const model = await Model.create(
         process.env["DB_HOST"] ?? "localhost",
         dbUsername,
@@ -21,28 +25,39 @@ async function main() {
     ));
     botApi.help(c => c.replyWithMarkdownV2(
         "Comandi disponibili:\n"
-         + "• /in: usalo una sola volta per ricere le notifiche anche con il gruppo in silenzioso\n"
-         + "• /out: usalo una sola volta per **NON** ricevere più le notifiche anche con il gruppo in silezioso\n"
+         + "• /in: usalo una sola volta per ricevere le notifiche anche con il gruppo in silenzioso\n"
+         + "• /out: usalo una sola volta per *NON* ricevere più le notifiche anche con il gruppo in silezioso\n"
          + "• /raid: usalo per generare una notifica automatica per un raid specificando, nell'ordine, il nome del Pokémon e il tempo rimanente"
+         + "Questo bot funziona solamente per i gruppi Telegram, non per chat private."
     ));
     botApi.command("in", async c => {
+        if (!["group", "supergroup"].includes(c.chat.type)) {
+            return await c.reply("Mi dispiace, questa funzione è disponibile solamente nei gruppi e nei supergruppi");
+        }
         const name = c.from.username ?? c.from.first_name;
         if (await model.insertUser(c.from.id, name, c.chat.id) > 0) {
-            c.reply(`Ho inserito ${name} tra chi notificare!`);
+            return await c.reply(`Ho inserito ${name} tra chi notificare!`);
         }
+        return Promise.resolve();
     });
     botApi.command("out", async c => {
-        if (await model.deleteUser(c.from.id, c.chat.id) > 0) {
-            c.reply(`Ho rimosso ${c.from.username ?? c.from.first_name} da chi notificare!`);
+        if (!["group", "supergroup"].includes(c.chat.type)) {
+            return await c.reply("Mi dispiace, questa funzione è disponibile solamente nei gruppi e nei supergruppi");
         }
+        if (await model.deleteUser(c.from.id, c.chat.id) > 0) {
+            return await c.reply(`Ho rimosso ${c.from.username ?? c.from.first_name} da chi notificare!`);
+        }
+        return Promise.resolve();
     });
     botApi.command("raid", async c => {
+        if (!["group", "supergroup"].includes(c.chat.type)) {
+            return await c.reply("Mi dispiace, questa funzione è disponibile solamente nei gruppi e nei supergruppi");
+        }
         const [ pokemonName, minutes ]: string[] = c.args;
         if (!pokemonName || !minutes) {
-            c.reply("Al messaggio manca qualcosa!");
-            return;
+            return await c.reply("Al messaggio manca qualcosa!");
         }
-        c.reply(
+        return await c.reply(
             `Un nuovo raid per ${pokemonName} è iniziato e mancano ${minutes} minuti alla fine!\n`.concat(
                 ...(await model.getAllUsers(c.chat.id)).map(u => `@${u}\n`),
                 "!!!"
@@ -65,4 +80,4 @@ async function main() {
     });
 }
 
-main().then(console.log).catch(console.error);
+main().then(() => console.log("Ready")).catch(console.error);
