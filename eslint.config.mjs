9c7bfce0709a86ec722de8bd1d4b8c718803d4d6
Matteo Castellucci <matteo.castellucci@outlook.com>
2024-07-29 23:47:01 +0200

    build: add configurations for next framework

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,15 +3,27 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
-import eslintConfigSonarJS from "eslint-plugin-sonarjs";
+import sonarjs from "eslint-plugin-sonarjs";
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+});
 
 export default tseslint.config({
-    files: ["**/*.ts"],
+    files: ["**/*.{ts,tsx,mtsx}"],
+    ignores: [".next/**"],
     extends: [
         eslint.configs.recommended,
         ...tseslint.configs.strictTypeChecked,
         ...tseslint.configs.stylisticTypeChecked,
-        eslintConfigSonarJS.configs.recommended,
+        sonarjs.configs.recommended,
+        ...compat.extends("plugin:@next/next/core-web-vitals"),
         eslintConfigPrettier,
     ],
     languageOptions: {
